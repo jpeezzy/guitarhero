7707b57756ad42a8ab72354891f959f240f7e70b
jpeezzy <jpeezzy@hotmail.com>
2017-03-28 18:49:32 -0700

    revised code, got wammy bar working

--- soundtest.cpp
+++ soundtest.cpp
@@ -15,39 +15,9 @@
 #include <conio.h>
 
 using namespace std;
-void playwav(std::string filename) {
-	int device = -1;
-	int freq = 44100;
-	const void* file = filename.c_str();
-	HSAMPLE streamHandle;
-	HCHANNEL channel;
-	/* Initialize output device */
-	BASS_Init(device, freq, BASS_DEVICE_DEFAULT, 0, NULL);
-	streamHandle = BASS_SampleLoad(false, file, 0, 0, 1, BASS_SAMPLE_MONO | BASS_SAMPLE_OVER_POS | BASS_SAMPLE_OVER_VOL);
-		channel = BASS_SampleGetChannel(streamHandle, FALSE);
-		/* Once you are done with your sample you should free it, but we don't wnat to until the play sample is over, or else nothing happens */
-		BASS_ChannelPlay(channel, FALSE);
-		//place all files in same directory or else shit goes down just put all files in resouces even though you're not using anything
-		//http://www.cplusplus.com/forum/windows/26944/
-	/* Load sampleza and play it */	
-}
 
-struct input {
-	char button;
-	char strum;
-};
-input input1, input2;
-
-std::map <WORD, string> getfilename(){
-	std::map <WORD, string> wavfile;
-	wavfile[XINPUT_GAMEPAD_A + XINPUT_GAMEPAD_DPAD_DOWN] = "D.Wav"; //in terms of button mapping this is GREEN
-	wavfile[XINPUT_GAMEPAD_B + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // RED
-	wavfile[XINPUT_GAMEPAD_X + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // BLUE 
-	wavfile[XINPUT_GAMEPAD_Y + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // YELLOW 
-	wavfile[XINPUT_GAMEPAD_LEFT_SHOULDER + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; //Orange doesn't have adefault. Its 0x100 or 256
-	wavfile[XINPUT_GAMEPAD_DPAD_DOWN + XINPUT_GAMEPAD_DPAD_DOWN] = ""; //down flipper ^_^
-	return wavfile; 
-};
+void playwav(std::string filename); 
+std::map <WORD, string> getfilename();
 int main()
 {
 	_XINPUT_GAMEPAD gamepad;
@@ -56,38 +26,60 @@ int main()
 	{
 		XINPUT_STATE state;
 		ZeroMemory(&state, sizeof(XINPUT_STATE));
-
 	    // Simply get the state of the controller from XInput.
 		dwResult = XInputGetState(i, &state);
-
 		if (dwResult == ERROR_SUCCESS)
-		{
 			cout << "Controller is connected" << endl;
-
-		}
 		else
-		{
 			cout << "Controller is not connected" << endl;
-		}
-	}
-	
+	}	
 
-	XINPUT_STATE state;
-	ZeroMemory(&state, sizeof(XINPUT_STATE));
+	XINPUT_STATE state; //declare state object from xinput_state struct
+	ZeroMemory(&state, sizeof(XINPUT_STATE)); //finding memory location of zero memory
 	std::map <WORD, string> roar = getfilename();
-	input* ptoin1;
-	ptoin1 = &input1;
-	int i = 0;
+	WORD wammy = 0x0; 
 	while (1) {
-		//cout << "does it go here?";
-	//	cout << "here too ??? ";
+		 // need to implement this for more wav file functionality 
 		dwResult = XInputGetState(0, &state);
-	//	if (state.Gamepad.wButtons == XINPUT_GAMEPAD_LEFT_SHOULDER) //orange left sholder
-		cout << state.Gamepad.wButtons << endl;
+		if (state.Gamepad.sThumbRX >= 0)
+			wammy = 0x0020;
+		else
+			wammy = 0x0;
+ //now we have new information. Wammy bar goes from -32768 (-0x8000) to 32767 (0x8000); member SHORT sThumbRX of struct Gamepad;
+		cout << state.Gamepad.wButtons + wammy << endl;
 		Sleep(100);
-		playwav(roar[state.Gamepad.wButtons]);
+		playwav(roar[state.Gamepad.wButtons + wammy]);
 		;
 	}
 return 0;
+}
+
+std::map <WORD, string> getfilename() {
+	std::map <WORD, string> wavfile;
+	wavfile[XINPUT_GAMEPAD_A + XINPUT_GAMEPAD_DPAD_DOWN] = "D.Wav"; //in terms of button mapping this is GREEN
+	wavfile[XINPUT_GAMEPAD_B + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // RED
+	wavfile[XINPUT_GAMEPAD_X + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // BLUE 
+	wavfile[XINPUT_GAMEPAD_Y + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; // YELLOW 
+	wavfile[XINPUT_GAMEPAD_LEFT_SHOULDER + XINPUT_GAMEPAD_DPAD_DOWN] = "D.wav"; //Orange doesn't have adefault. Its 0x100 or 256
+	wavfile[XINPUT_GAMEPAD_DPAD_DOWN + XINPUT_GAMEPAD_DPAD_DOWN] = ""; //down flipper ^_^
+	wavfile[XINPUT_GAMEPAD_A + XINPUT_GAMEPAD_DPAD_DOWN + 0x0020] = "D.Wav";//wammy file
+	return wavfile;
+};
+
+void playwav(std::string filename){
+	int device = -1;
+	int freq = 44100;
+	const void* file = filename.c_str();
+	HSAMPLE streamHandle;
+	HCHANNEL channel;
+	/* Initialize output device */
+	BASS_Init(device, freq, BASS_DEVICE_DEFAULT, 0, NULL);
+	streamHandle = BASS_SampleLoad(false, file, 0, 0, 3, BASS_SAMPLE_MONO | BASS_SAMPLE_OVER_POS | BASS_SAMPLE_OVER_VOL);
+	channel = BASS_SampleGetChannel(streamHandle, FALSE);
+	/* Once you are done with your sample you should free it, but we don't wnat to until the play sample is over, or else nothing happens */
+	BASS_ChannelPlay(channel, FALSE);
+	//place all files in same directory or else shit goes down just put all files in resouces even though you're not using anything
+	//http://www.cplusplus.com/forum/windows/26944/
+	/* Load sampleza and play it */
 }
 
